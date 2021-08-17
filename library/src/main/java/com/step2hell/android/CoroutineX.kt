9afd562894da13ef750739509c73e7b6c6c52e4e
yangbo <yangbo243@jd.com>
2021-08-17 18:41:19 +0800

    feat: 增加协程相关扩展方法

--- library/src/main/java/com/step2hell/android/CoroutineX.kt
+++ library/src/main/java/com/step2hell/android/CoroutineX.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 inline fun <T> CoroutineScope.launch(
     context: CoroutineContext = CoroutineExceptionHandler { _, throwable -> onFailure(throwable) },
@@ -29,3 +30,20 @@ inline fun <T> ViewModel.launch(
     crossinline onSuccess: (T?) -> Unit = {},
     crossinline onFailure: (Throwable) -> Unit = {}
 ): Job = viewModelScope.launch(context, start, action, onStart, onSuccess, onFailure)
+
+private val jobs: MutableMap<String, Job> = mutableMapOf()
+fun Job.cancelLast(): Job = apply {
+    val method = Thread.currentThread().stackTrace[4].methodName
+    val lineNumber = Thread.currentThread().stackTrace[4].lineNumber
+    val tag = method + lineNumber
+    jobs[tag]?.cancel()
+    jobs[tag] = apply {
+        invokeOnCompletion {
+            jobs.remove(tag)
+        }
+    }
+}
+
+val coroutineExceptionHandler: ((Throwable) -> Unit).() -> CoroutineExceptionHandler = {
+    CoroutineExceptionHandler { _, throwable -> this(throwable) }
+}
