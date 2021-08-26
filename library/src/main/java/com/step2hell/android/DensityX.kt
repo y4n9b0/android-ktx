77cab479f933078c0986d0109a951cecd37ee060
yangbo <yangbo243@jd.com>
2021-08-26 10:51:58 +0800

    feat: Density转化增加value class的思路

--- library/src/main/java/com/step2hell/android/DensityX.kt
+++ library/src/main/java/com/step2hell/android/DensityX.kt
@@ -29,3 +29,53 @@ val STATUS_BAR_HEIGHT: Int
         val resId = system.getIdentifier("status_bar_height", "dimen", "android")
         return if (resId != 0) system.getDimensionPixelSize(resId) else 24.dp2px
     }
+
+/**
+ * 10.dp2x          使用更加方便，但是污染整个 Number
+ * Dp(10).toPx()    使用 value class 包装避免了对 Number 的污染，但是使用更加繁琐
+ *
+ * 综合比较，还是推荐使用前者，毕竟我们扩展的初衷就是为了使用更加方便。
+ * 以下几个 value class Sp 和 Px 类似。
+ */
+@Deprecated(
+    message = "Inconvenient to use",
+    replaceWith = ReplaceWith("Number.dp2px"),
+    level = DeprecationLevel.HIDDEN
+)
+@JvmInline
+value class Dp(private val n: Number) {
+    fun toPx(): Int {
+        return n.toFloat().times(metrics.density).roundToInt()
+    }
+}
+
+@Deprecated(
+    message = "Inconvenient to use",
+    replaceWith = ReplaceWith("Number.sp2px"),
+    level = DeprecationLevel.HIDDEN
+)
+@JvmInline
+value class Sp(private val n: Number) {
+    fun toPx(): Int {
+        val metrics = Resources.getSystem().displayMetrics
+        return n.toFloat().times(metrics.scaledDensity).roundToInt()
+    }
+}
+
+@Deprecated(
+    message = "Inconvenient to use",
+    replaceWith = ReplaceWith("Number.px2dp or Number.ps2sp"),
+    level = DeprecationLevel.HIDDEN
+)
+@JvmInline
+value class Px(private val n: Number) {
+    fun toDp(): Float {
+        val metrics = Resources.getSystem().displayMetrics
+        return n.toFloat().div(metrics.density)
+    }
+
+    fun toSp(): Float {
+        val metrics = Resources.getSystem().displayMetrics
+        return n.toFloat().div(metrics.scaledDensity)
+    }
+}
