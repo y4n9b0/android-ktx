f080fbf6a6d9c917fd13ddf774c2fd1212a7edf3
ᴮᵒᵇ <step2hell@qq.com>
2021-10-29 13:47:57 +0800

    feat: 修复 Toast 官方的 BadTokenException

--- library/src/main/java/com/step2hell/android/ToastX.kt
+++ library/src/main/java/com/step2hell/android/ToastX.kt
@@ -1,7 +1,13 @@
 package com.step2hell.android
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.annotation.IntDef
 import androidx.annotation.StringRes
@@ -11,10 +17,41 @@ import androidx.annotation.StringRes
 internal annotation class Duration
 
 @Throws(Resources.NotFoundException::class)
-fun Context.toast(@StringRes resId: Int, @Duration duration: Int = Toast.LENGTH_SHORT): Context = apply {
-    Toast.makeText(this, resId, duration).show()
-}
+fun Context.toast(@StringRes resId: Int, @Duration duration: Int = Toast.LENGTH_SHORT): Context =
+    apply {
+        Toast.makeText(this, resId, duration).ignoreBadToken().show()
+    }
 
-fun Context.toast(text: CharSequence, @Duration duration: Int = Toast.LENGTH_SHORT): Context = apply {
-    Toast.makeText(this, text, duration).show()
+fun Context.toast(text: CharSequence, @Duration duration: Int = Toast.LENGTH_SHORT): Context =
+    apply {
+        Toast.makeText(this, text, duration).ignoreBadToken().show()
+    }
+
+@SuppressLint("DiscouragedPrivateApi")
+fun Toast.ignoreBadToken(): Toast = apply {
+    if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1) {
+        try {
+            val tnField = Toast::class.java.getDeclaredField("mTN").apply { isAccessible = true }
+            val tn = tnField.get(this)
+
+            val handlerField = tnField.type.getDeclaredField("mHandler").apply {
+                isAccessible = true
+            }
+            val handler = handlerField.get(tn) as Handler
+
+            val looper = checkNotNull(Looper.myLooper()) {
+                "Can't toast on a thread that has not called Looper.prepare()"
+            }
+            handlerField.set(tn, object : Handler(looper) {
+                override fun handleMessage(msg: Message) {
+                    try {
+                        handler.handleMessage(msg)
+                    } catch (ignored: WindowManager.BadTokenException) {
+                    }
+                }
+            })
+        } catch (ignored: IllegalAccessException) {
+        } catch (ignored: NoSuchFieldException) {
+        }
+    }
 }
